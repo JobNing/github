80f9cb714816e13d8c0298c1a0f8afe47ce2ac9e
qq178149 <1781499903@qq.com>
2019-05-10 19:57:48 +0800

    swoole

--- swoole.php
+++ swoole.php
@@ -1,12 +1,15 @@
 <?php
 
-    $ser = new swoole_server("0.0.0.0",9501);
-    $ser->set(array(
-       'worker_num'=>4,
-       'daemonize'=>true,
-        'backlog'=>128
-    ));
-    $ser->on('connect',function ($ser,$fd){
-        echo "success";
-    });
+class swoole {
+    private $serv;
+    public function __construct()
+    {
+        $this->serv = new swoole_server("0.0.0.0",9501);
+    }
+    public function start(){
+        $this->serv->on('start',function ($server){
+            echo 123;
+        });
+    }
+}
 
